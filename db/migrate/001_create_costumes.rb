577ff97897d14b2c628df1f7ca4a8b6af3527873
dwane richards <dwanerich@gmail.com>
2020-03-14 02:18:46 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,7 +2,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   
   def change
     create_table :costumes do |t|
-      t.string:name
+      t.string :name
       t.float :price
       t.string :size
       t.string :image_url
@@ -18,15 +18,3 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
 end
 
 # ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => "db/costumes.sqlite")
-
-
-
-# Create a class and inherit from ActiveRecord::Migration
-
-# By convention, the class name should match the part of the
-# file name after the number, so in this case:
-# 002_create_costumes.rb becomes class CreateCostumes
-
-# Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
-# value types according to the spec
